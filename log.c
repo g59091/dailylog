ae9d5e15093b52cde3fc38f87c367aada613a353
G59091 <agrian142476@gmail.com>
2018-11-19 02:50:18 -0500

    initial log

---
+++ log.c
@@ -0,0 +1,41 @@
+// Simple Loging C Program
+// Made by Lamsten Corporation
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+  FILE *fptr;
+  char filename[100], sentence[100], fnbuf[100], *timecheck;
+  char am[] = "AM", pm[] = "PM";
+  time_t rawtime;
+  time(&rawtime);
+  struct tm *currentdate = localtime(&rawtime);
+  int sec = currentdate->tm_sec;
+  int min = currentdate->tm_min;
+  int hour = currentdate->tm_hour;
+  int day = currentdate->tm_mday;
+  int mon = (currentdate->tm_mon) + 1;
+  char *mont[12] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+  int year = (currentdate->tm_year) + 1900;
+  
+  printf("The time now is the %dth of %s, %d.\n"
+  		 "Log anything you need to. Type \"===\" to quit.\n", day, mont[mon - 1], year);
+  
+  snprintf(fnbuf, sizeof(fnbuf), "log%d%d%d.txt", day, mon, year - 2000);
+  fptr = fopen(fnbuf, "a+");
+  //snprintf(timecheck, sizeof(timecheck), "%d:%d:%d %s", hour % 12, min, sec, (hour >= 12) ? "PM" : "AM"); 
+  //printf(timecheck);
+  while (strcmp(gets(sentence), "===") != 0) 
+  {	
+  	//fputs(timecheck, fptr);
+  	fputs("\n", fptr);
+    fputs(sentence, fptr);
+    fputs("\n", fptr);
+  }
+  fclose(fptr);
+  printf("File closed and saved.");
+  
+  return 0;
+}
